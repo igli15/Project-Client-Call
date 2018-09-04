0264c3adb02d21d14f53408139df751f945564c3
August <patrickpanda7@gmail.com>
2018-09-04 15:51:52 +0200

    RangedAttack,Movement etc.

---
+++ Project Client Call/Assets/_Scripts/EnemyScripts/EnemyAI.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(EnemyMovement))]
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField]
+    PlayerData playerData;
+
+    private Transform playerTransform
+    {
+        get { return playerData.transform; }
+    }
+    private EnemyMovement enemyMovement;
+    private EnemyRangedAttack enemyRangedAttack;
+    void Start()
+    {
+        enemyRangedAttack = GetComponent<EnemyRangedAttack>();
+        enemyMovement = GetComponent<EnemyMovement>();
+    }
+
+    private void FixedUpdate()
+    {
+        enemyRangedAttack.ShootTo(playerTransform.position);
+        //Avoid(playerTransform);
+    }
+
+    private float GetHorizontalDirectionTo(Transform targetTransform)
+    {
+        float _horizontalDirection = targetTransform.position.x - transform.position.x;
+        if (_horizontalDirection > 1) _horizontalDirection = 1;
+        if (_horizontalDirection < -1) _horizontalDirection = -1;
+        return _horizontalDirection;
+    }
+    private void Follow(Transform targetTransform)
+    {
+        float _horizontalDirection=GetHorizontalDirectionTo(targetTransform);
+        enemyMovement.Move(_horizontalDirection, 0);
+    }
+
+    private void Avoid(Transform targetTransform)
+    {
+        float _horizontalDirection = GetHorizontalDirectionTo(targetTransform);
+        enemyMovement.Move( -_horizontalDirection, 0); // negative direction
+    }
+
+}
