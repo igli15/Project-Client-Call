fe14416eeef8e15d1067f7fbc64c0d17f9d93a83
Igli milaqi <milaqiigli@gmail.com>
2018-09-04 11:42:10 +0200

    Started working on the prototype. Organised folders. Note: Make sure to be in prototype branch if you want to commit smth for the prototype.

---
+++ Project Client Call/Assets/_Scripts/PlayerData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ *  Use this class to store all player flieds. example: movement speed, jump speed etc....
+ *  All fields are private and can be changed by setters only.
+ *  No behaviour/functions go here expept setting the player values.
+ *  Look At movement speed as an exmaple.
+ */
+
+public class PlayerData : MonoBehaviour
+{
+    [SerializeField]
+    private float movementSpeed = 2;
+
+    public static Action<PlayerData> OnMovementSpeedChanged;
+
+    public void ChangeMovementSpeed(int value)
+    {
+        if (OnMovementSpeedChanged != null) OnMovementSpeedChanged(this);
+        movementSpeed += value;
+    }
+
+}
