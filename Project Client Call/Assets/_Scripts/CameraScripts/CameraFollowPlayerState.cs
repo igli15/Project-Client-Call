647c7f2e331f6442a21af8848ca5da4516e9ba4a
Igli milaqi <milaqiigli@gmail.com>
2018-09-06 22:05:14 +0200

    Fixed some camera behaviour bugs, now it should work as needed.

--- Project Client Call/Assets/_Scripts/CameraScripts/CameraFollowPlayerState.cs
+++ Project Client Call/Assets/_Scripts/CameraScripts/CameraFollowPlayerState.cs
@@ -17,65 +17,73 @@ public class CameraFollowPlayerState : AbstractState<CameraFsmController>
 
 	private bool followPlayer = false;
 
-	private bool init = false;
-	// Use this for initialization
-	void Start ()
-	{
-	
-		
-	}
+	private bool movingToTarget = false;
 
 	public override void Enter(IAgent pAgent)
 	{
 		base.Enter(pAgent);
-		init = false;
+		movingToTarget = false;
 		
-		if (targetToFollow.transform.position.x >= transform.position.x && !init)
-		{
-
-			Vector3 stageDimensions = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
-			transform.DOLocalMoveX((transform.position.x + (targetToFollow.transform.position.x - transform.position.x)) + (targetToFollow.position.x -stageDimensions.x - 0.5f),0.2f);			
-
-			init = true;
-			Invoke("InvokeFollowPlayer",1.5f);
-		}
+		FindRightPosBehindTarget();
 	}
 
 	private void LateUpdate()
 	{
-		if (targetToFollow.transform.position.x >= transform.position.x && !init)
+		if (targetToFollow.transform.position.x >= transform.position.x && !movingToTarget)  //check if in the middle or more to the right..
 		{
-			offset = transform.position - targetToFollow.position;
+			offset = transform.position - targetToFollow.position;  //Get the right offset from the target to the camera
+			
 			followPlayer = true;
-			init = true;
-
+			movingToTarget = true; //just so we do this once
 		}
 
 		if (followPlayer)
 		{
-			float desiredPos = targetToFollow.position.x + offset.x;
 
-			Vector3 smoothedPos = Vector3.Lerp(transform.position,
-				new Vector3(desiredPos, transform.position.y, transform.position.z),
-				smoothedSpeed * Time.deltaTime);
-
-			transform.position = smoothedPos;
+			StartFollowingPlayer();
 		}
 		
 		
 	}
 
-	private void InvokeFollowPlayer()
+	private void SetMovingToTargetFalse()
 	{
-		offset = Vector3.zero;
-		offset = transform.position - targetToFollow.position;
-		followPlayer = true;
+		movingToTarget = false;
 	}
 
 	public override void Exit(IAgent pAgent)
 	{
 		base.Exit(pAgent);
 		followPlayer = false;
-		init = false;
+		movingToTarget = false;
+	}
+
+	public void FindRightPosBehindTarget()
+	{
+		if (targetToFollow.transform.position.x >= transform.position.x && !movingToTarget)
+		{
+
+			Vector3 stageDimensions = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));  //get stage dimensions
+			
+			transform.DOLocalMoveX((transform.position.x + 
+			                        (targetToFollow.transform.position.x - transform.position.x)) 
+			                       + (targetToFollow.position.x -stageDimensions.x - 1.5f),0.2f);	 // move behind target		
+
+			movingToTarget = true; //So we don't overlap tweens
+			
+			Invoke("SetMovingToTargetFalse",1.5f);    //We want to check again but only after the tweener has finished 
+													 //So that animations/Tweens do not overlap.
+		}
+	}
+
+	public void StartFollowingPlayer()
+	{
+		float desiredPos = targetToFollow.position.x + offset.x;
+
+		Vector3 smoothedPos = Vector3.Lerp(transform.position,
+			new Vector3(desiredPos, transform.position.y, transform.position.z),
+			smoothedSpeed * Time.deltaTime);
+
+		transform.position = smoothedPos;
 	}
 }
