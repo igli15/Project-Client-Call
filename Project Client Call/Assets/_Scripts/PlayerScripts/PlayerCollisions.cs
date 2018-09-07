a28ed28bb7896c6a50a9326eec7cbeaa617b4904
Igli milaqi <milaqiigli@gmail.com>
2018-09-07 14:53:56 +0200

    Added arena triggers to test the camera.

---
+++ Project Client Call/Assets/_Scripts/PlayerScripts/PlayerCollisions.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollisions : MonoBehaviour
+{
+
+	private Camera mainCam;
+	
+	private void Start()
+	{
+		mainCam = Camera.main;
+	}
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.CompareTag("ArenaExitTrigger"))
+		{
+			mainCam.GetComponent<CameraFsmController>().fsm.ChangeState<CameraFollowPlayerState>();
+		}
+		if (other.CompareTag("ArenaEnterTrigger"))
+		{
+			mainCam.GetComponent<CameraFsmController>().fsm.ChangeState<CameraArenaState>();
+		}
+	}
+}
