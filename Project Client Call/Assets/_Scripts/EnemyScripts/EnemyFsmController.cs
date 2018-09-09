e9e07e87d1e6719d682731016ec035e846e72ef6
August <patrickpanda7@gmail.com>
2018-09-09 18:08:30 +0200

    Add IsLooking, IsInVision (added fewLayers, added Tag to Avatar(childOfPlayer) tag  "Player")

--- Project Client Call/Assets/_Scripts/EnemyScripts/EnemyFsmController.cs
+++ Project Client Call/Assets/_Scripts/EnemyScripts/EnemyFsmController.cs
@@ -25,8 +25,20 @@ public class EnemyFsmController : MonoBehaviour, IAgent
 
     private void Update()
     {
+        Vector2 distanceToPLayer = enemyData.Player.transform.position - transform.position;
+        float _horizontalDirectionToPlayer = Mathf.Sign(distanceToPLayer.x);
 
-        if ((enemyData.Player.transform.position - transform.position).magnitude < radiusOfShooting)
+        int layerMask = 1 << 10 | (1 << 9);
+        RaycastHit2D raycast2d = Physics2D.Raycast(transform.position, distanceToPLayer, radiusOfShooting, layerMask);
+
+        bool isLooking = transform.right.x==_horizontalDirectionToPlayer;
+        bool isInVision= raycast2d.collider != null && raycast2d.collider.CompareTag("Player");
+
+        Debug.DrawRay(transform.position, distanceToPLayer);
+
+        
+
+        if ((distanceToPLayer).magnitude < radiusOfShooting && isLooking&& isInVision)
         {
             GetComponent<EnemyFsmController>().fsm.ChangeState<EnemyRangedAttackState>();
         }
