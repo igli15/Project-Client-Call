6c18910eccdb8bfdfa0546a8185238b02d97b55b
August <patrickpanda7@gmail.com>
2018-09-10 19:30:49 +0200

    Added Health to enemies (damage from bullet is 1, max HP is 1)

---
+++ Project Client Call/Assets/_Scripts/BulletScripts/BulletCollision.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletCollision : MonoBehaviour
+{
+    [SerializeField]
+    float damage = 1;
+
+	private Rigidbody2D rb;
+    [HideInInspector]
+    public bool isReflected;
+	private EnemyRangedAttack enemyRangedAttack;
+	
+	private void Start()
+	{
+        isReflected = false;
+		rb = GetComponent<Rigidbody2D>();
+		enemyRangedAttack = GameObject.FindGameObjectWithTag("Enemy").GetComponent<EnemyRangedAttack>();
+	}
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.CompareTag("Sword Collider"))
+		{
+			if(rb != null)
+			rb.velocity = other.transform.parent.right * enemyRangedAttack.GetBulletSpeed() * TimeManager.timeSlowScale ;
+            isReflected = true;
+		}
+
+		if (other.transform.CompareTag("Player"))
+		{
+			Destroy(gameObject);
+		}
+        if (isReflected && other.transform.CompareTag("Enemy"))
+        {
+            other.GetComponent<Health>().InflictDamage(damage);
+            Destroy(gameObject);
+        }
+        
+	}
+
+	private void OnCollisionEnter(Collision other)
+	{
+		
+	}
+}
