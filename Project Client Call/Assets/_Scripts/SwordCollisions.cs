8420551cc6e30e20bd8087e946555cef4ea95ca8
Igli milaqi <milaqiigli@gmail.com>
2018-09-10 12:38:11 +0200

    Renamed "Rotate with mouse script" to "Sword Rotation".

---
+++ Project Client Call/Assets/_Scripts/SwordCollisions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordCollisions : MonoBehaviour
+{
+
+	[SerializeField] 
+	private Rigidbody2D playerRb;
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		
+	}
+}
--- Project Client Call/Assets/_Scripts/SwordRotation.cs
+++ Project Client Call/Assets/_Scripts/SwordRotation.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RotateWithMouse : MonoBehaviour
+public class SwordRotation : MonoBehaviour
 {
 
 	[SerializeField]
