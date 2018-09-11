f2e91dc5584376f8ba37b78bc520e084015da564
Igli milaqi <milaqiigli@gmail.com>
2018-09-11 23:30:24 +0200

    Added a pretty reusable selesctable system.

---
+++ Project Client Call/Assets/_Scripts/HudScripts/SelectableManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class SelectableManager : MonoBehaviour
+{
+	[SerializeField] 
+	protected int MaxNumberOfSelectables;
+	
+	public static ISelectable[] Selectables;
+
+	[HideInInspector] 
+	public static int currentIndex;
+	
+	// Use this for initialization
+
+	private void Awake()
+	{
+		Selectables = new ISelectable[MaxNumberOfSelectables];
+	}
+
+	void Start ()
+	{
+		currentIndex = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetKeyDown(KeyCode.LeftArrow)) ShiftSelect(false);
+		else if(Input.GetKeyDown(KeyCode.RightArrow)) ShiftSelect(true);
+	}
+
+	public void ShiftSelect(bool right)
+	{
+		if (right)
+		{
+			if (currentIndex >= Selectables.Length - 1)
+			{
+				Debug.Log("reached the end(right)");
+				currentIndex = 0;
+				return;
+			}
+
+			currentIndex += 1;
+
+		}
+		else
+		{
+			if (currentIndex <= 0)
+			{
+				Debug.Log("reached the end(left)");
+				currentIndex = Selectables.Length - 1;
+				return;
+			}
+
+			currentIndex -= 1;
+		}
+	}
+
+	public ISelectable GetCurrentSelected()
+	{
+		return Selectables[currentIndex];
+	}
+
+	protected void AddSelectable(int index,ISelectable selectable)
+	{
+		Selectables[index] = selectable;
+	}
+}
---
+++ Project Client Call/Assets/_Scripts/HudScripts/WordScroll.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WordScroll : SelectableManager,ISelectable
+{
+
+	[SerializeField] 
+	private int indexToBePlaced = 0;
+	
+	private bool isSelected = false;
+
+	private Image image;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		AddSelectable(indexToBePlaced,this);
+
+		image = GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (GetCurrentSelected().Equals(this) && !isSelected)
+		{
+			BeSelected();
+			Debug.Log(currentIndex);
+		}
+		else if (!GetCurrentSelected().Equals(this))
+		{
+			Reset();
+		}
+
+	}
+
+	public void BeSelected()
+	{
+		image.color = Color.black;
+		isSelected = true;
+	}
+
+	public void Reset()
+	{
+		image.color = Color.white;
+		isSelected = false;
+	}
+}
