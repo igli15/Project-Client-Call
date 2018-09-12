a642ed09521fdf708cc6f9aaf8c871107e21accb
Guillaume Kingham <guil.king@t-online.de>
2018-09-12 09:51:03 +0200

    Added new folders and scripts for menu and audio

---
+++ Project Client Call/Assets/_Scripts/AudioScripts/IntroScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+public class IntroScript : MonoBehaviour {
+
+	// Use this for initialization
+	public VideoPlayer introPlayer;
+	void Start () {
+		introPlayer.loopPointReached += hasEnded;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.KeypadEnter))
+		{
+			Camera.main.enabled = true;
+			this.gameObject.SetActive(false);
+		}
+	}
+
+	void hasEnded(VideoPlayer pIntroPlayer)
+	{
+		Camera.main.enabled = true;
+		this.gameObject.SetActive(false);
+	}
+}
