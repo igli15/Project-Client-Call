87640e88b8205ef0d92f50b456c7b723e22ab991
Igli milaqi <milaqiigli@gmail.com>
2018-09-12 20:12:30 +0200

    Added Encryption and save and load basic behaviours.

---
+++ Project Client Call/Assets/_Scripts/SaveLoadScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class SaveLoadScript : MonoBehaviour {
+
+	public void Save(PlayerCollisions sender = null)
+	{
+		var data = JsonUtility.ToJson(this, true);
+		File.WriteAllText(Application.persistentDataPath + "/SaveData.json", Encryption.Encrypt(data));
+	}
+    
+	public void Load()
+	{
+		if (File.Exists(Application.persistentDataPath + "/SaveData.json"))
+		{
+			var loadedData = Encryption.Decrypt(File.ReadAllText(Application.persistentDataPath + "/PlayerData.json"));
+			Debug.Log(loadedData);
+			JsonUtility.FromJsonOverwrite(loadedData, this);
+		}
+	}
+}
