edc6687854d845e4020ff22953ab543fda6ee8dc
Guillaume Kingham <guil.king@t-online.de>
2018-09-12 13:32:07 +0200

    menu things

---
+++ Project Client Call/Assets/InsertCoinScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InsertCoinScript : MonoBehaviour {
+
+	private Text _text;
+	[SerializeField] private float SizeDifference;
+	[SerializeField] private float SizeIncrease;
+	// Use this for initialization
+	void Start () {
+		_text = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//frewpkfweioj
+	}
+}
--- Project Client Call/Assets/menuScript.cs
+++ Project Client Call/Assets/menuScript.cs
@@ -4,26 +4,66 @@ using UnityEngine;
 using UnityEngine.UI;
 public class menuScript : MonoBehaviour {
 
-	// Use this for initialization
 	
 	[SerializeField] private Image _stroke;
 	[SerializeField] private Button[] button = new Button[4];
-	private int buttonCount;
+	[SerializeField] private float scrollSpeed;
+	private float scrollAmount;
+	private int selectedButton;
 	void Start () {
-		buttonCount = 0;
+		scrollAmount = 1;
+		selectedButton = 0;
+		button[selectedButton].GetComponentInChildren<Text>().color = Color.white;
+		Debug.Log(_stroke.transform.position);
+		Debug.Log(button[0].transform.position);
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		if(Input.GetKeyDown(KeyCode.DownArrow))
+		
+		if(scrollAmount < 0)
 		{
-			buttonCount += 1;
-			/*stroke.rectTransform.position = button[buttonCount].transform.position;*/
-			//_stroke.GetComponent<Animator>().Play("stroke", 0, 0);
+			if(Input.GetAxisRaw("VerticalJoy") > 0 | Input.GetAxisRaw("Vertical") > 0)
+			{
+				Debug.Log(button.Length);
+				if(selectedButton > 0)
+				{
+					button[selectedButton].GetComponentInChildren<Text>().color = Color.black;
+					selectedButton -= 1;
+					_stroke.transform.position = button[selectedButton].transform.position;
+					button[selectedButton].GetComponentInChildren<Text>().color = Color.white;
+					scrollAmount = 1;
+				}
+				else 
+				{
+					button[selectedButton].GetComponentInChildren<Text>().color = Color.black;
+					selectedButton = button.Length -1;
+					_stroke.transform.position = button[selectedButton].transform.position;
+					button[selectedButton].GetComponentInChildren<Text>().color = Color.white;
+					scrollAmount = 1;
+				}
+			}
+
+			if(Input.GetAxisRaw("VerticalJoy") < 0 | Input.GetAxis("Vertical") < 0)
+			{
+				Debug.Log("hey2!!!");
+					if(selectedButton < button.Length - 1)
+					{
+						button[selectedButton].GetComponentInChildren<Text>().color = Color.black;
+						selectedButton += 1;
+						_stroke.transform.position = button[selectedButton].transform.position;
+						button[selectedButton].GetComponentInChildren<Text>().color = Color.white;
+						scrollAmount = 1;
+					}
+					else 
+					{
+						button[selectedButton].GetComponentInChildren<Text>().color = Color.black;
+						selectedButton = 0;
+						_stroke.transform.position = button[selectedButton].transform.position;
+						button[selectedButton].GetComponentInChildren<Text>().color = Color.white;
+						scrollAmount = 1;
+					}
+			}
 		}
-		if(Input.GetKeyDown(KeyCode.UpArrow))
-		{
-			
-		}
+		scrollAmount -= scrollSpeed;
 	}
 }
