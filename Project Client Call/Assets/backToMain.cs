17bda94258d78804971d82bbb99118ac9401b601
Guillaume Kingham <guil.king@t-online.de>
2018-09-13 12:43:10 +0200

    Menu Complete, only preloading left and scoreboard aaaaaannd credits... yap

---
+++ Project Client Call/Assets/backToMain.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class backToMain : MonoBehaviour {
+
+	[SerializeField] private GameObject menu;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.KeypadEnter))
+		{
+			menu.SetActive(true);
+			this.gameObject.SetActive(false);
+		}
+	}
+}
