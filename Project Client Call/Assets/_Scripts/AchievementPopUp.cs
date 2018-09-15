8f0b56f03ada848e3a25b53301e5d086da73189f
Igli milaqi <milaqiigli@gmail.com>
2018-09-15 17:57:41 +0200

    Made sure achievements can be queued.

--- Project Client Call/Assets/_Scripts/AchievementPopUp.cs
+++ Project Client Call/Assets/_Scripts/AchievementPopUp.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class AchievementPopUp : MonoBehaviour
-{	
+{
+	[Header("PopUpElements")] 
+	[SerializeField]
+	private Text titleText;
+
+	[SerializeField]
+	private Text description;
+
+	[SerializeField] 
+	private Text japaneseText;
+	
+	[SerializeField] 
+	private Image backgroundImage;
+	
 	[Serializable]
 	public class AchievementData
 	{
@@ -15,12 +29,21 @@ public class AchievementPopUp : MonoBehaviour
 		public string japaneseText;
 		//public Sprite icon;
 		public Sprite background;
+
+		[HideInInspector] 
+		public bool isCompleted;
 	}
 
+	[Space]
+	[Header("PopUpDatas")]
 	[SerializeField] private List<AchievementData> achievementData;
 
 	private Dictionary<string, AchievementData> achievementDictionary;
 	private RectTransform rectTransform;
+	
+	private Queue<AchievementData> achievementQueue = new Queue<AchievementData>();
+
+	private bool isDisplaying = false;
 		
 	// Use this for initialization
 	void Start ()
@@ -32,32 +55,59 @@ public class AchievementPopUp : MonoBehaviour
 		
 		foreach (AchievementData data in achievementData)
 		{
-			//achievementDictionary.Add(data.title,);
+			achievementDictionary.Add(data.title,data);
 		}
 	}
 
 
-	public void Show(AchievementData pData = null)
+	public void Show(AchievementData pData)
 	{
+		backgroundImage.sprite = pData.background;
+		japaneseText.text = pData.japaneseText;
+		description.text = pData.description;
+		titleText.text = pData.title;
+		
+		
 		rectTransform.DOAnchorPos(new Vector2(0,450), 0.8f);
+
+		isDisplaying = true;
+		pData.isCompleted = true;
+		StartCoroutine(Reset(pData));
 	}
 
-	public void Reset(AchievementData pData = null)
+	IEnumerator Reset(AchievementData pData)
 	{
-
+		yield return  new WaitForSeconds(2f);
+		
 		rectTransform.DOAnchorPos(new Vector2(0 + rectTransform.sizeDelta.x, 450), 0.8f);
+		DOVirtual.DelayedCall(0.8f,() => isDisplaying =false);
 	}
 
 	private void Update()
 	{
+		if (achievementQueue.Count() > 0)
+		{
+			if (!isDisplaying)
+			{
+				Show(achievementQueue.Dequeue());
+			}
+		}
 		if (Input.GetKeyDown(KeyCode.A))
 		{
-			Show();
+			QueueAchievement("The Beginning");
+		}
+		if (Input.GetKeyDown(KeyCode.B))
+		{
+			QueueAchievement("Jump");
 		}
 
-		if (Input.GetKeyDown(KeyCode.S))
+	}
+
+	public void QueueAchievement(string achievementName)
+	{
+		if (!achievementDictionary[achievementName].isCompleted)
 		{
-			Reset();
+			achievementQueue.Enqueue(achievementDictionary[achievementName]);
 		}
 	}
 }
