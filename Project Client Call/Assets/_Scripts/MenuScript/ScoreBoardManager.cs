6117eef012585f48f3bdef2592531da11b2b8227
Igli milaqi <milaqiigli@gmail.com>
2018-09-17 13:19:44 +0200

    changed highscore overlap.

--- Project Client Call/Assets/_Scripts/MenuScript/ScoreBoardManager.cs
+++ Project Client Call/Assets/_Scripts/MenuScript/ScoreBoardManager.cs
@@ -8,24 +8,28 @@ public class ScoreBoardManager : MonoBehaviour
 {
 	[SerializeField] 
 	private Text highscoreTextField;
+
+	[SerializeField] 
+	private Text nameTextField;
 	
 	// Use this for initialization
 	private void OnEnable()
 	{
-		highscoreTextField.text = GenerateHighscore();
+		highscoreTextField.text = "";
+		nameTextField.text = "";
+		GenerateHighscore();
 	}
 
-	private string GenerateHighscore()
+	private void GenerateHighscore()
 	{
-		string generatedString = " ";
 		
 		HighScoreManager.instance.highscoreArray = HighScoreManager.instance.highscoreArray.OrderByDescending(x => x.score).ToArray();
 		
 		for (int i = 0; i < HighScoreManager.instance.highscoreArray.Length; i++)
 		{
-			generatedString += i+1 + "-  " + HighScoreManager.instance.highscoreArray[i].name + ":  " +
-			                   HighScoreManager.instance.highscoreArray[i].score + "\n";
+			nameTextField.text += HighScoreManager.instance.highscoreArray[i].name + "\n";
+			highscoreTextField.text += HighScoreManager.instance.highscoreArray[i].score + "\n";
 		}
-		return generatedString;
+		
 	}
 }
