3019171ba4767b179fb6b77ac593deb0a544214e
Igli milaqi <milaqiigli@gmail.com>
2018-09-18 20:47:41 +0200

    Added bloom effects while in slow mo.

--- Project Client Call/Assets/PostProccesingManager.cs
+++ Project Client Call/Assets/PostProccesingManager.cs
@@ -6,27 +6,53 @@ using UnityEngine.PostProcessing;
 
 public class PostProccesingManager : MonoBehaviour
 {
-
-	private PostProcessingProfile currentCC;
-
+	[Header("VignetterSettings")]
 	[SerializeField]
 	private VignetteModel.Settings vignetteSettings;
 	[SerializeField]
 	private float maxVignetteIntensity = 1;
+
+	[SerializeField] 
+	private float vignetteIncreaseAmount = 5;
 	
+	[Space]
+	
+	[Header("BloomSettings")]
+	[SerializeField]
+	private BloomModel.Settings bloomSettings;
+	[SerializeField]
+	private float maxBloomIntensity = 1;
 
+	[SerializeField] 
+	private float bloomIncreaseAmount = 5;
+
+	private PostProcessingProfile currentCC;
+	
+	//Vignette private fields
 	private VignetteModel.Settings initVignetteSetting;
-
 	private bool increaseVignetteIntensity;
-
-	private bool stop;
+	private bool stopIncreasingVignette;
+	
+	//bloom private fields
+	private BloomModel.Settings initBloomSetting;
+	private bool increaseBloomIntensity;
+	private bool stopIncreasingBloom;
+	
 	
 	// Use this for initialization
 	void Start ()
 	{
 		currentCC = GetComponent<PostProcessingBehaviour>().profile;
+		
+		currentCC.vignette.settings = vignetteSettings;
+		currentCC.bloom.settings = bloomSettings;
+		
 		initVignetteSetting = currentCC.vignette.settings;
-
+		initBloomSetting = currentCC.bloom.settings;
+		
+		vignetteIncreaseAmount /= 1000;
+		bloomIncreaseAmount /= 1000;
+		
 		PlayerSlowMotionState.OnDeflectionStateEntered += StartIncreasingVignetteIntensity;
 		
 		PlayerSlowMotionState.OnDeflectionStateExit += StopIncreasingVignetteIntensity;
@@ -37,28 +63,53 @@ public class PostProccesingManager : MonoBehaviour
 	void Update ()
 	{
 		VignetteBehaviour();
+		BloomBehaviour();
 	}
 
 	private void VignetteBehaviour()
 	{
-		if (increaseVignetteIntensity && currentCC.vignette.settings.intensity <= maxVignetteIntensity && !stop)
+		if (increaseVignetteIntensity && currentCC.vignette.settings.intensity <= maxVignetteIntensity && !stopIncreasingVignette)
 		{
 
-			vignetteSettings.intensity += 0.005f;
+			vignetteSettings.intensity += vignetteIncreaseAmount;
 			currentCC.vignette.settings = vignetteSettings;
 			
 			if (currentCC.vignette.settings.intensity >= maxVignetteIntensity)
 			{
-				stop = true;
+				stopIncreasingVignette = true;
 			}
 			
 		}
 		
 		if(currentCC.vignette.settings.intensity > initVignetteSetting.intensity && !increaseVignetteIntensity)
 		{
-			vignetteSettings.intensity -= 0.005f;
+			vignetteSettings.intensity -= vignetteIncreaseAmount;
 			currentCC.vignette.settings = vignetteSettings;
-			stop = false;
+			stopIncreasingVignette = false;
+		}
+
+	}
+	
+	private void BloomBehaviour()
+	{
+		if (increaseBloomIntensity && currentCC.bloom.settings.bloom.intensity <= maxBloomIntensity && !stopIncreasingBloom)
+		{
+
+			bloomSettings.bloom.intensity += bloomIncreaseAmount;
+			currentCC.bloom.settings = bloomSettings;
+			
+			if (currentCC.bloom.settings.bloom.intensity >= maxBloomIntensity)
+			{
+				stopIncreasingBloom = true;
+			}
+			
+		}
+		
+		if(currentCC.bloom.settings.bloom.intensity > initBloomSetting.bloom.intensity && !increaseBloomIntensity)
+		{
+			bloomSettings.bloom.intensity -= bloomIncreaseAmount;
+			currentCC.bloom.settings = bloomSettings;
+			stopIncreasingBloom = false;
 		}
 
 	}
@@ -66,11 +117,13 @@ public class PostProccesingManager : MonoBehaviour
 	private void StartIncreasingVignetteIntensity(PlayerSlowMotionState sender)
 	{
 		increaseVignetteIntensity = true;
+		increaseBloomIntensity = true;
 	}
 	
 	private void StopIncreasingVignetteIntensity(PlayerSlowMotionState sender)
 	{
 		increaseVignetteIntensity = false;
+		increaseBloomIntensity = false;
 	}
 
 	private void OnDestroy()
