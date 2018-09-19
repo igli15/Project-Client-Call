ede92acfe73d5d536dec0b3a06f50373f99ade57
Igli milaqi <milaqiigli@gmail.com>
2018-09-19 12:23:53 +0200

    Renamed BulletHAlo script To => LightIntensityManager

--- Project Client Call/Assets/_Scripts/LightIntensityManager.cs
+++ Project Client Call/Assets/_Scripts/LightIntensityManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
 
-public class BulletHalo : MonoBehaviour
+public class LightIntensityManager : MonoBehaviour
 {
 	[SerializeField] 
 	private float timeTillMaxIntensity = 0.2f;
