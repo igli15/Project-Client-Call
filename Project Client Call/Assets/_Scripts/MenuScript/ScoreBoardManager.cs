7f72f54ce54069671f0b6aeff0246456bb6f722d
August <patrickpanda7@gmail.com>
2018-09-20 02:04:26 +0200

    fixes

--- Project Client Call/Assets/_Scripts/MenuScript/ScoreBoardManager.cs
+++ Project Client Call/Assets/_Scripts/MenuScript/ScoreBoardManager.cs
@@ -19,19 +19,26 @@ public class ScoreBoardManager : MonoBehaviour
 	{
 		highscoreTextField.text = "";
 		nameTextField.text = "";
-		GenerateHighscore();
+
+        GenerateHighscore();
 	}
 
 	private void GenerateHighscore()
 	{
-		
-		HighScoreManager.instance.highscoreArray = HighScoreManager.instance.highscoreArray.OrderByDescending(x => x.score).ToArray();
-		HighScoreManager.instance.highscoreArray = CutArray(HighScoreManager.instance.highscoreArray,6);
-		for (int i = 0; i < HighScoreManager.instance.highscoreArray.Length; i++)
-		{
-			nameTextField.text += HighScoreManager.instance.highscoreArray[i].name + "\n";
-			highscoreTextField.text += HighScoreManager.instance.highscoreArray[i].score + "\n";
-		}
+        if (HighScoreManager.instance.highscoreArray != null)
+        {
+            HighScoreManager.instance.highscoreArray = HighScoreManager.instance.highscoreArray.OrderByDescending(x => x.score).ToArray();
+            HighScoreManager.instance.highscoreArray = CutArray(HighScoreManager.instance.highscoreArray, 6);
+            for (int i = 0; i < HighScoreManager.instance.highscoreArray.Length; i++)
+            {
+                if (HighScoreManager.instance.highscoreArray[i] != null)
+                {
+                    nameTextField.text += HighScoreManager.instance.highscoreArray[i].name + "\n";
+                    highscoreTextField.text += HighScoreManager.instance.highscoreArray[i].score + "\n";
+                }
+
+            }
+        }
 	}
 
 	private HighScoreManager.HighscoreData[] CutArray(HighScoreManager.HighscoreData[] arrayToCut,int MaxSize)
